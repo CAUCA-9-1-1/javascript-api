f60e09ea8a031b31ab65bfd3a7cdeefd05d336ae
michael.jolin <michael.jolin@cauca.ca>
2017-11-10 09:44:58 -0500

    SWI-158 Modifier le script de compression pour la nouvelle structure

--- compress.sh
+++ compress.sh
@@ -5,20 +5,20 @@
 
 # Compress all javascript file in one
 
-cat ./cause/js/uncompressed/*.js > ./cause/js/cause.js
-cat ./cause/js/uncompressed/client/*.js >> ./cause/js/cause.js
-cat ./cause/js/uncompressed/devExtreme/*.js >> ./cause/js/cause.js
-cat ./cause/js/uncompressed/externalPlugins/*.js >> ./cause/js/cause.js
-cat ./cause/js/uncompressed/localization/*.js >> ./cause/js/cause.js
-cat ./cause/js/uncompressed/viewer/*.js >> ./cause/js/cause.js
+cat ./js/uncompressed/*.js > ./cause/js/cause.js
+cat ./js/uncompressed/client/*.js >> ./cause/js/cause.js
+cat ./js/uncompressed/devExtreme/*.js >> ./cause/js/cause.js
+cat ./js/uncompressed/externalPlugins/*.js >> ./cause/js/cause.js
+cat ./js/uncompressed/localization/*.js >> ./cause/js/cause.js
+cat ./js/uncompressed/viewer/*.js >> ./cause/js/cause.js
 
 # Uglify the compressed file
-cat ./cause/js/cause.js | uglifyjs -o ./cause/js/cause.min.js
+cat ./js/cause.js | uglifyjs -o ./js/cause.min.js
 
 # Generate the docs
 if [ "$#" -eq 1 ]; then
-    rm -rf ./cause/docs/*.html
-    jsdoc ./cause/js/cause.js -u ./cause/docs/tutorials -d ./cause/docs -r ./cause/README.md
+    rm -rf ./docs/*.html
+    jsdoc ./js/cause.js -u ./docs/tutorials -d ./docs -r ./README.md
 else
     echo "Pass a argument to compile the docs! (./compress docs)"
 fi
